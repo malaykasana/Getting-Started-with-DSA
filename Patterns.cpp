32b45dd44f086f0d3620b048fdbe3cdf55ce8e2b
malaykasana <malaykasana_se_25b02029@dtu.ac.in>
2026-01-09 14:07:36 +0530

    Edit Patterns

--- Patterns.cpp
+++ Patterns.cpp
@@ -10,7 +10,7 @@ void pattern1(int m, int n){
     }
 }
 
-void pattern2 (int m, int n){
+void pattern2 (int m){
     for (int i{0}; i<m; i++){
         for (int j{0};j<i+1;j++){
             cout<<j+1<<" ";
@@ -19,9 +19,28 @@ void pattern2 (int m, int n){
     }
 }
 
+void pattern3(int m){
+    for (int i{0};i<m;i++){
+        for (int j{0};j<i+1;j++){
+            cout<<i+1<<" ";
+        }
+        cout<<'\n';
+    }
+}
+
+void pattern4(int m){
+    for (int i{m};i>0;i--){
+        for (int j{0};j<i;j++){
+            cout<<j+1<<" ";
+        }
+        cout<<'\n';
+
+    }
+}
 int main() {
     //pattern1(5,5);
-    pattern2(5,5);
-
+    //pattern2(5);
+    //pattern3(5);
+    pattern4(5);
     return 0;
 }
