14c139f330c0afcb07be8f1c26a7da0b572b48e3
malaykasana <malaykasana_se_25b02029@dtu.ac.in>
2026-03-24 15:34:52 +0530

    a bit medium

---
+++ Arrays/rotate matrix by 90.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// brute o(n2) o(n2)
+
+// int main() {
+//     int m=3, n=3;
+//     int arr[m * n]{
+//         1, 2, 3,
+//         4, 5, 6,
+//         7, 8, 9
+//     };
+//     int temp[m*n]{};
+//     for (int i{} ; i<m ; ++i){
+//         for (int j{} ; j<n ; ++j){
+//             temp[i*n+j]=arr[(m-j-1)*n+i];
+//         }
+//     }
+//     for (int i{} ; i<m*n ; ++i){
+//         if (i%3==0) cout << '\n';
+//         cout << temp[i] << ' ';
+//     }
+//     return 0;
+// }
+
+// optimal o(n2) o(1)
+// transpose and reverse the matrix
+
+int main(){
+    int m=3, n=3;
+    int arr[m * n]{
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+    };
+    for (int i{} ; i<m ; ++i){
+        for (int j{i+1} ; j<n ; ++j){
+            swap(arr[i*n+j],arr[j*m+i]);
+        }
+    }
+    for (int i{} ; i<m ; ++i){
+        reverse(arr+i*n,arr+i*n+n);
+    }
+    for (int i{} ; i<m*n ; ++i){
+        if (i%3==0) cout << '\n';
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
