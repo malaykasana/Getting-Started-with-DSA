b210bdd599ff814bd26a3d2bf9ab6de19b665ae0
malaykasana <malaykasana_se_25b02029@dtu.ac.in>
2026-03-24 19:15:55 +0530

    hard with 1d array

---
+++ Arrays/spiral traversal of matrix.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m = 4, n = 4;
+    int arr[m*n]{ 
+        1,   2,  3, 4,
+        5,   6,  7, 8,
+        9,  10, 11, 12,
+        13, 14, 15, 16
+    };
+    int top = 0, bottom = m-1, left = 0, right = n-1;
+    while (top<=bottom && left<=right){
+        for (int i{left} ; i<=right ; ++i){
+            cout << arr[top*n+i] << ' ';
+        }
+        ++top;
+        for (int i{top} ; i<=bottom ; ++i){
+            cout << arr[i*n+right] << ' ';
+        }
+        --right;
+        if (top<=bottom){
+            for (int i{right} ; i>=left ; --i){
+                cout << arr[bottom*n+i] << ' ';
+            }
+            --bottom;
+        }
+        if (left<=right){
+            for (int i{bottom} ; i>=top ; --i){
+                cout << arr[i*n+left] << ' ';
+            }
+            ++left;
+        }
+    }
+    return 0;
+}
